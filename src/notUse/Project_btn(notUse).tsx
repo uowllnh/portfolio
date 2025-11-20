ca8b1de097d87df3c5cb5b86996206e7564d995a
uowllnh <uowllnh@gmail.com>
2025-11-21 00:24:16 +0900

    rename: notUse 분류

---
+++ src/notUse/Project_btn(notUse).tsx
@@ -0,0 +1,51 @@
+import { useMemo} from "react";
+
+type Tab = "personal" | "team";
+
+type Project = {
+  id: string;
+  title: string;
+  desc: string;
+  type: Tab;
+  href?: string;
+};
+
+const PROJECTS: Project[] = [
+  { id: "yoring",       title: "Yoring",       desc: "스마트폰 요금제 추천 앱",    type: "team" },
+  { id: "metaplanner",  title: "Meta Planner", desc: "뽀모도로 기반 일정관리 앱",  type:  "team"},
+  { id: "storykid",     title: "StoryKid",     desc: "AI 동화 생성 웹 서비스",     type: "team" },
+  { id: "uxlab",        title: "Portfolio Site",       desc: "포트폴리오 웹 사이트",     type: "personal" },
+];
+
+export default function Projects({ selected }: { selected: Tab }) {
+  const list = useMemo(() => PROJECTS.filter(p => p.type === selected), [selected]);
+
+  if (list.length === 0) {
+    return <section className="py-8 text-center text-gray-400">표시할 프로젝트가 없습니다.</section>;
+  }
+
+return ( 
+<section className="section-scroll"> 
+{/* 래퍼만 위치 지정 */} 
+<div className="scroll-container px-14 snap-x snap-mandatory  [&::-webkit-scrollbar]:hidden scrollbar-none"> 
+  
+  {list.map((p,i) => ( 
+    <section
+    key={i}
+          className="snap-start shrink-0 flex"
+        >
+
+        <div className="flex w-[760px] justify-center">
+       <div className="project-btn top-[40%]"> 
+        <div className="project-btn-txt"> 
+          <h1 className="text-3xl font-semibold">{p.title}</h1> 
+          <h2 className="text-2xl">{p.desc}</h2> 
+          </div> 
+          </div> 
+          </div>
+          </section> 
+          ))} 
+          </div> 
+          </section> );
+
+}
---
+++ src/notUse/project_detail(notUse).tsx
@@ -0,0 +1,45 @@
+import Project_detail from "../components/project_template(notUse)";
+
+export default function project_detail(click : string) {
+
+
+<div>
+<Project_detail
+title = "Yoring"
+description = "사용자 맞춤형 요금제 추천 앱"
+ member = "FE 1명"
+ detail = "사용자에게 맞춰 요금제를 추천합니다"
+ period = "2025.00.00 ~ 2026.00.00" />
+
+
+ <Project_detail
+title = "Meta Planner"
+description = "사용자 맞춤형 요금제 추천 앱"
+ member = "FE 1명"
+ detail = "사용자에게 맞춰 요금제를 추천합니다"
+ period = "2025.00.00 ~ 2026.00.00" />
+
+
+ <Project_detail
+title = "StoryKid"
+description = "사용자 맞춤형 요금제 추천 앱"
+ member = "FE 1명"
+ detail = "사용자에게 맞춰 요금제를 추천합니다"
+ period = "2025.00.00 ~ 2026.00.00" />
+
+
+ <Project_detail
+title = "Portfolio Site"
+description = "사용자 맞춤형 요금제 추천 앱"
+ member = "FE 1명"
+ detail = "사용자에게 맞춰 요금제를 추천합니다"
+ period = "2025.00.00 ~ 2026.00.00" />
+
+
+</div>
+
+
+
+
+
+ }
